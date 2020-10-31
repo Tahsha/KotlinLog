ff49a1d6b3b4ffc3b1d72c422facb43941e0ead2
takakoi <lifedesign.chris@gmail.com>
2020-10-31 17:16:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -0,0 +1,15 @@
+package jp.techacademy.taro.kirameki.kotlinlog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log   // ここを追加
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("kotlintest", "ログへの出力テスト")    // ここを追加
+    }
+}
